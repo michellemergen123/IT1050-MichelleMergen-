74957781e09ac2aa9006d6f0a8a2c650d14d8c82
michelle mergen <michelle5720@gmail.com>
2018-10-26 17:52:19 -0400

    Completed lab01

---
+++ Lab01/Lab01/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Hello World
+using System;
+class Solution
+{
+    private const string Value = "Hello World from Michelle Mergen!";
+
+    static void Main(String[] args)
+    {
+        Console.WriteLine(Value);
+    }
+}
